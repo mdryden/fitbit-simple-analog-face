d69e6cf9a4a24a7124dc7d2f6bb503b490145588
Michael Dryden <mjdryden@gmail.com>
2022-02-11 16:57:46 -0600

    Implemented digital time, then disabled

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ let secondhand = document.getElementById("secondhand");
 let batteryPercent = document.getElementById("battery");
 let lowBatteryPercent = document.getElementById("batteryLow");
 let dayOfMonth = document.getElementById("dayOfMonth");
+let digitalTime = document.getElementById("digitalTime");
 
 clock.granularity = "seconds";
 let currentDayOfMonth = -1;
@@ -17,12 +18,12 @@ let currentMinutes = -1;
 let batteryChargeLevel = -1;
 
 clock.ontick = tick => {
-  adjustHands(tick.date);
+  adjustTime(tick.date);
   adjustDate(tick.date);
   adjustBattery();
 };
 
-function adjustHands(date) {
+function adjustTime(date) {
   if (date.getMinutes() !== currentMinutes) {
     currentMinutes = date.getMinutes();
     hourhand.groupTransform.rotate.angle = 30 * (date.getHours() % 12) + 0.5 * currentMinutes;
@@ -30,6 +31,17 @@ function adjustHands(date) {
 
   minutehand.groupTransform.rotate.angle = 6 * currentMinutes + 0.1 * date.getSeconds();
   secondhand.groupTransform.rotate.angle = 6 * date.getSeconds();
+
+  // let hours = date.getHours();
+  // let minutes = date.getMinutes();
+  // let marker = "AM";
+
+  // if (hours > 12) {
+  //   hours = hours - 12;
+  //   marker = "PM";
+  // }
+
+  // digitalTime.text = `${hours}:${minutes} ${marker}`;
 }
 
 function adjustDate(date) {
